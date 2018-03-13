643d9ddb2f293c80059e5d5830c8a8b0604c312c
Paul Hine <paul.hine@gmail.com>
2018-03-13 16:38:42 +0100

    build for universal

--- src/components/SubjectImage/index.stories.js
+++ src/components/SubjectImage/index.stories.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Image, View } from 'react-native'
 import { storiesOf } from '@storybook/react-native'
 import SubjectImage from './'
-import { mixins } from '../../shared-styles'
+import { mixins, variables } from '../../shared-styles'
 
 const containerStyle = {
   ...mixins.centerBoth,
+  height: '100%',
+  backgroundColor: variables.colors.appBackground,
 }
 
 const Decorate = ({ story }) => <View style={containerStyle}>{story}</View>
@@ -14,11 +16,11 @@ storiesOf('SubjectImage', module)
   .addDecorator(story => <Decorate story={story()} />)
   .add('default', () => (
     <SubjectImage>
-      <Image source={{ uri: 'https://www.fillmurray.com/300/200.png' }} />
+      <Image source={{ uri: 'http://www.fillmurray.com/300/200.png' }} />
     </SubjectImage>
   ))
   .add('active', () => (
     <SubjectImage isActive>
-      <Image source={{ uri: 'https://www.fillmurray.com/300/200.png' }} />
+      <Image source={{ uri: 'http:///www.fillmurray.com/300/200.png' }} />
     </SubjectImage>
   ))
